c6c00cc3b5aa827c30aa40d44921bbb115b4c204
fytta <rafa.esparza.1995@gmail.com>
2023-09-18 22:13:38 +0200

    game over example 1

---
+++ GameOver/GameOver.cpp
@@ -0,0 +1,14 @@
+// GameOver.cpp
+// First C++ program
+
+#include <iostream>
+using std::cout, std::endl;
+//using namespace std;
+
+int main()
+{
+    //std::cout << "Game Over!" << std::endl;
+    cout << "Game Over!" << endl;
+    
+    return 0;
+}
